77daf68d5a3eb449b3150b02df27447e7e668fa1
Lakshika Sabharwal <122308077+lakkuu04@users.noreply.github.com>
2025-07-30 16:21:51 +0530

    Update Average_Of_Three.java

--- Function_Assignment/Average_Of_Three.java
+++ Function_Assignment/Average_Of_Three.java
@@ -5,4 +5,16 @@ public class Function{
   public static int averageOfThree(int a,int b,int c){
     int avg = (a + b + c)/3 ;
     return avg ;
+  }
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    int l = sc.nextInt();
+    int m = sc.nextInt();
+    int n = sc.nextInt();
+
+    int result = averageOfThree(l,m,n);
+
+    System.out.prinln("Average of Three : "+result);
+  }
+} 
     
