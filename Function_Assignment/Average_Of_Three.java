369b7367d961d082a6e5e419be7af92cbc6eabfa
Lakshika Sabharwal <122308077+lakkuu04@users.noreply.github.com>
2025-07-30 16:18:16 +0530

    Create Average_Of_Three.java

---
+++ Function_Assignment/Average_Of_Three.java
@@ -0,0 +1,8 @@
+/**Enter 3 numbers from the user & make a function to print their average.*/
+
+import java.util.*;
+public class Function{
+  public static int averageOfThree(int a,int b,int c){
+    int avg = (a + b + c)/3 ;
+    return avg ;
+    
