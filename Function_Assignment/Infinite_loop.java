ed0bcbe5be919cce26c0e7f46331038774b861a5
Lakshika Sabharwal <122308077+lakkuu04@users.noreply.github.com>
2025-07-30 17:22:15 +0530

    Create Infinite_loop.java

---
+++ Function_Assignment/Infinite_loop.java
@@ -0,0 +1,14 @@
+/** Write an infinite loop using do while condition. */
+
+import java.util.*;
+public class functions {
+    public static void Looping(int n){
+        do{
+          System.out.println("Dummy !!!");
+        }while(n != 0);
+    }
+    public static void main(String []args){
+      Looping(1);
+    }
+}
+      
